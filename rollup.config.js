ebb4cab803e4496e214648a8229ae5195fa37908
Darnell Arford <ugadarnell@gmail.com>
2018-07-17 18:56:18 -0400

    Change config to output CommonJS code instead of ES6

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import babel from 'rollup-plugin-babel'
 export default {
   entry: 'src/index.js',
   dest: 'dist/bundle.js',
-  format: 'es',
+  format: 'cjs',
   external: ['babel-polyfill', 'ramda', 'reselect'],
   plugins: [
     babel({
